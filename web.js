e7fd0c148f1a67880198af7d5bfa8b75d7041545
Manuel Orozco <manuel.orozco@wanadoo.fr>
2013-07-11 08:08:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
 var textfile = fs.readFile (index.html);
 console.log(textfile);
  response.send('Hello World 2!');
