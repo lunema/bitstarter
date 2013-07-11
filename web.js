b0972f906c83bd2914e7b65bb230b322f20b8bc3
Manuel Orozco <manuel.orozco@wanadoo.fr>
2013-07-11 07:52:54 +0000

    index html included 2

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs');
 var textfile = fs.readFile (index.html);
 console.log(textfile);
  response.send('Hello World 2!');
