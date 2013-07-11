6c630df9cb67e85ad10aa94fe860c7ba266e439d
Manuel Orozco <manuel.orozco@wanadoo.fr>
2013-07-11 07:38:52 +0000

    index html included

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var textfile = fs.readFile (index.html);
+console.log(textfile);
+ response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
